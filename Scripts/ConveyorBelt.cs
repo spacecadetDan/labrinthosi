79322a0f9b93d86ba2e150af3cb0f7fe9f4cbf8d
marchiadevana <52432976+marchiadevana@users.noreply.github.com>
2019-07-11 11:47:52 +1000

    Scripts for the scene
    To get the conveyor belt running and the world following it

---
+++ Scripts/ConveyorBelt.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour
+{
+    public GameObject belt;
+    public Transform endPoint;
+    public float speed;
+    // Start is called before the first frame update
+    void OnTriggerStay(Collider other)
+    {
+        other.transform.position = Vector3.MoveTowards(other.transform.position, endPoint.position, speed * Time.deltaTime);
+    }
+}
