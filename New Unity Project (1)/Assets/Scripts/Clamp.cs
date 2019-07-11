8ff450768b9cd7c090813f6001e834162ad95db5
Marchia Devana <marchiadevana@gmail.com>
2019-07-11 16:14:54 +1000

    Production Line app first commit

---
+++ New Unity Project (1)/Assets/Scripts/Clamp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Clamp : MonoBehaviour
+{
+    public TextMesh nameLabel;
+    public Transform end;
+    public float speed;
+    void Update()
+    {
+        //Vector3 namePos = gameObject.transform.position;
+        Debug.Log(this.transform.position);
+        nameLabel.transform.position = Vector3.MoveTowards(this.transform.position, end.position, speed * Time.deltaTime);
+        //nameLabel.transform.position = namePos;
+    }
+}
